455c179082075e33a859cd011972d3553bba2557
Ryan Layer <ryan.layer@gmail.com>
2016-01-08 11:31:16 -0700

    allow subset of tests to run

--- example_tests.sh
+++ example_tests.sh
@@ -6,86 +6,94 @@ STOP_ON_FAIL=0
 
 #--- assert_in_stdout
 # Success
-run python -c "print 'example assert_in_stdout success'"
+run assert_in_stdout python -c "print 'example assert_in_stdout success'"
 assert_in_stdout "example" $LINENO
 
 # Fails because there is nothing in stdout
-run python -c "import sys; sys.stderr.write('example assert_in_stdout failure')"
+run assert_in_stdout python -c "import sys; sys.stderr.write('example assert_in_stdout failure')"
 assert_in_stdout "example" $LINENO
 
 # Fails because "simple test" is not in stdout
-run python -c "print 'example assert_in_stdout success'"
+run assert_in_stdout python -c "print 'example assert_in_stdout success'"
 assert_in_stdout "simple test" $LINENO
 
 
 #--- assert_in_stderr
 # Success
-run python -c "import sys; sys.stderr.write('example assert_in_stderr success')"
+run assert_in_stderr python -c "import sys; sys.stderr.write('example assert_in_stderr success')"
 assert_in_stderr "example" $LINENO
 
 # Fails because there is nothing in stderr
-run python -c "print 'example assert_in_stderr failure'"
+run assert_in_stderr python -c "print 'example assert_in_stderr failure'"
 assert_in_stderr "example" $LINENO
 
 # Fails because "simple test" is not in stderr
-run python -c "import sys; sys.stderr.write('example assert_in_stderr success')"
+run assert_in_stderr python -c "import sys; sys.stderr.write('example assert_in_stderr success')"
 assert_in_stderr "simple test" $LINENO
 
 #--- assert_stdout
 # Success
-run python -c "print 'example assert_stdout success'"
+run assert_in_stdout python -c "print 'example assert_stdout success'"
 assert_stdout $LINENO
 
 # Fails because there is nothing in stdout
-run python -c "import sys; sys.stderr.write('example assert_stdout failure')"
+run assert_in_stdout python -c "import sys; sys.stderr.write('example assert_stdout failure')"
 assert_stdout $LINENO
 
 #--- assert_stderr
 # Success
-run python -c "import sys; sys.stderr.write('example assert_stderr success')"
+run assert_stderr python -c "import sys; sys.stderr.write('example assert_stderr success')"
 assert_stderr $LINENO
 
 # Fails because there is nothing in stderr
-run python -c "print 'example assert_stderr failure'"
+run assert_stderr python -c "print 'example assert_stderr failure'"
 assert_stderr $LINENO
 
 #assert_no_stdout
 # Success
-run python -c "import sys; sys.stderr.write('example assert_no_stdout success')"
+run assert_no_stdout python -c "import sys; sys.stderr.write('example assert_no_stdout success')"
 assert_no_stdout $LINENO
 # Fails because there is something in stdout
-run python -c "print 'example assert_no_stdout failure'"
+run assert_no_stdout python -c "print 'example assert_no_stdout failure'"
 assert_no_stdout $LINENO
 
 #-- assert_no_stderr
 # Success
-run python -c "print 'example assert_no_stderr success'"
+run assert_no_stderr python -c "print 'example assert_no_stderr success'"
 assert_no_stderr $LINENO
 # Fails because there is something in stderr
-run python -c "import sys; sys.stderr.write('example assert_no_stderr failure')"
+run assert_no_stderr python -c "import sys; sys.stderr.write('example assert_no_stderr failure')"
 assert_no_stderr $LINENO
 
 #--assert_exit_code
 # Success
-run python -c "print 1;"
+run assert_exit_code python -c "print 1;"
 assert_exit_code $EX_OK $LINENO
-run python -c "import sys; sys.exit(66);"
+run assert_exit_code python -c "import sys; sys.exit(66);"
 assert_exit_code $EX_NOINPUT $LINENO
-
 # Fails because the error code is wrong
-run python -c "import sys; sys.exit(66);"
+run assert_exit_code python -c "import sys; sys.exit(66);"
 assert_exit_code $EX_USAGE $LINENO
 
 #assert_fail_to_stderr
 # Success
-run python -c "import sys; sys.stderr.write('example assert_no_stderr failure');sys.exit(66)"
+run assert_fail_to_stderr python -c "import sys; sys.stderr.write('example assert_no_stderr failure');sys.exit(66)"
 assert_fail_to_stderr $EX_NOINPUT $LINENO
 # Fails because the exit code is wrong
-run python -c "import sys; sys.stderr.write('example assert_no_stderr failure');sys.exit(65)"
+run assert_fail_to_stderr python -c "import sys; sys.stderr.write('example assert_no_stderr failure');sys.exit(65)"
 assert_fail_to_stderr $EX_NOINPUT $LINENO
 # Fails because there is something in stdout
-run python -c "import sys; print 'something'; sys.stderr.write('example assert_no_stderr failure');sys.exit(66)"
+run assert_fail_to_stderr python -c "import sys; print 'something'; sys.stderr.write('example assert_no_stderr failure');sys.exit(66)"
 assert_fail_to_stderr $EX_NOINPUT $LINENO
 # Fails because there is nothing in stderr
-run python -c "import sys; print 'something'; sys.exit(66)"
+run assert_fail_to_stderr python -c "import sys; print 'something'; sys.exit(66)"
 assert_fail_to_stderr $EX_NOINPUT $LINENO
+
+#assert_equal
+# Success
+run assert_equal python -c "f = open('test.out','w'); f.write('1\n2\n3\n');f.close();"
+assert_equal "$(cat test.out | wc -l)" 3 $LINENO
+run assert_equal python -c "f = open('test.out','w'); f.write('1\n2\n3\n');f.close();"
+# Failure
+assert_equal "$(cat test.out | wc -l)" 4 $LINENO
+rm test.out
